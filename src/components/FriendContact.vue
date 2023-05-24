ec57693e394b7389a054dbaa412dd9f2b266891c
Zoe Cheng <zoecheng@evolutivelabs.com>
2023-05-24 16:00:26 +0800

    Vue3 practice

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,54 @@
+<template>
+    <li>
+        <h2> {{ name }}{{ isFavorite? "(Favorite)": "" }}</h2>
+        <button @click="toggleDetails">Show Details</button>
+        <button @click="toggleFavorite">Show Favorites</button>
+        <ul v-if="detailAreVisible">
+            <li><strong>Phone:</strong>{{ phone }}</li>
+            <li><strong>Email:</strong>{{ email }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    props:{
+        id: {
+            type:String,
+            required:true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        phone: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        isFavorite: {
+            type: Boolean,
+            required: false,
+            default: false
+        }
+    },
+    data() {
+        return {
+            detailAreVisible: false,
+        }
+    },
+    emits: ['toggle-favorite'],
+    methods: {
+        toggleDetails() {
+            return this.detailAreVisible = !this.detailAreVisible
+        },
+        toggleFavorite() {
+            this.$emit('toggle-favorite',this.id)
+        }
+
+    }   
+}
+</script>
---
+++ src/components/NewFriend.vue
@@ -0,0 +1,38 @@
+<template>
+    <form @submit.prevent="submitData">
+        <div>
+            <label>Name</label>
+            <input type="text" v-model="enterName"/>
+        </div>
+        <div>
+            <label>Phone</label>
+            <input type="tel" v-model="enterPhone"/>
+        </div>
+        <div>
+            <label>Email</label>
+            <input type="email" v-model="enterEmail"/>
+        </div>
+        <div>
+            <button>Add contact</button>
+        </div>
+    </form>
+    
+</template>
+
+<script>
+export default {
+    emits: ['add-friend'],
+    data() {
+        return {
+            enterPhone: '',
+            enterName: '',
+            enterEmail: ''
+        }
+    },
+    methods: {
+        submitData () {
+            this.$emit('add-friend', this.enterName, this.enterPhone, this.enterEmail)
+        }
+    }
+}
+</script>
